f26370bc81dc761e989c7ff8b3d01d75fe8d3561
DominikDolphin <dominik.thibaudeau@gmail.com>
2021-05-17 13:30:12 -0400

    Rays on Z axis now work as intended

--- src/server/handlePlacedBomb.server.lua
+++ src/server/handlePlacedBomb.server.lua
@@ -4,9 +4,8 @@ local tileSize = gameSettings:GetAttribute("tileSize"); -- constant value
 local bomb = game.ServerStorage:WaitForChild("Bomb")
 local Debris = game:GetService("Debris")
 
-
 local neighbours = {}
-local power = 2 --Users power
+local power = 3 --Users power
 
 local function testTouch()
     for _,v in pairs (neighbours) do
@@ -40,47 +39,41 @@ local function createExplosion(part)
     return exp;
 end
 
-local function handleNeighbours()
-    local getScripts = {}
-   -- local originalColor
-    for _,v in pairs (neighbours) do
-        --originalColor = v.BrickColor
-        v.BrickColor = BrickColor.new("Flint")
-        --table.insert(getScripts,v.killOnTouch)
+local function filterPlayers()
+    local players = {}
+    local getPlayers = game.Players:GetPlayers()
+    for _,v in pairs (getPlayers) do
+        table.insert(players,v.Character)
     end
-    wait (2)
 
-   
-    for _,v in pairs (neighbours) do
-        v.BrickColor = BrickColor.new("Black")
-        v:SetAttribute("killOnTouch", true)
-        v.killOnTouch.Disabled = false
-    end
-    wait(.4)
-    for _,v in pairs (neighbours) do
-        v:SetAttribute("killOnTouch", false)
-        v.BrickColor = BrickColor.Green()
-        v.killOnTouch.Disabled = true
-    end
-    table.clear(neighbours)
+    return players
 end
-
--- createBombRay(playerBomb, "UpVector")
-local function createBombRay(playerBomb, directionVector)
+-- createBombRay(playerBomb, "UpVector", orientation)
+local function createBombRay(playerBomb, directionVector, orientationNumber)
     local radius = 1
     local origin = playerBomb.Position
+    local powerRange = ((power * tileSize) + tileSize/2)
+    local orientation = orientationNumber or 1
 
     local direction
-
     if directionVector == "UpVector" then
-        direction = playerBomb.CFrame.UpVector * ((power*tileSize) + tileSize/2)
+        direction = playerBomb.CFrame.UpVector * powerRange * orientation
     else
-        direction =  playerBomb.CFrame.RightVector * (tileSize + tileSize/2)
+        direction =  playerBomb.CFrame.RightVector * powerRange * orientation
     end
    -- local direction = playerBomb.CFrame.UpVector * 100
 
+    --Ray will go through players
+    local playerCharacters = filterPlayers()
+
+    --List of things to filter out
    local raycastParams = RaycastParams.new()
-   raycastParams.FilterDescendantsInstances = {playerBomb, game.Players:GetPlayers(),workspace.Rays:GetChildren(), workspace.Tiles:GetChildren()}
+   raycastParams.FilterDescendantsInstances = {
+        playerBomb, 
+        playerCharacters,
+        workspace.Rays:GetChildren(),
+        workspace.Tiles:GetChildren()
+    }
     local midpoint = origin + direction/2
 
     local raycastResult = Workspace:Raycast(origin, direction, raycastParams)
@@ -95,20 +88,14 @@ local function createBombRay(playerBomb, directionVector)
         local hitPos = raycastResult.Position
         if hit and hitPos then
             hit.BrickColor = BrickColor.Red()
-            print(hit.Name)
-            print (hitPos)
+            
             local touchPartDirection = playerBomb.CFrame.UpVector * (((playerBomb.Position.Z + hitPos.Z)/2)-hitPos.Z/2)
             local newMid = origin + touchPartDirection/2
             killRay.CFrame = CFrame.new(newMid,origin)
             killRay.Size = Vector3.new(tileSize,radius, touchPartDirection.magnitude )
-            
-        else
-            print("Nothing.")
-        end
-    
-        
+        end  
     else -- It didnt hit anything
-        print("Didnt detect")
+        --print("Didnt detect")
         killRay.CFrame = CFrame.new(midpoint,origin)
         killRay.Size = Vector3.new(tileSize,radius, direction.magnitude)
     end
@@ -126,59 +113,15 @@ dropMelon.OnServerEvent:connect(function(player,hit) --Params from dropBomb.clie
         playerBomb.CFrame = hit.CFrame + Vector3.new(0,4,0)
         playerBomb.Rotation = Vector3.new(-90,0,0)
         --Debris:AddItem(playerBomb,2.1)
-        
-        -- local radius = 1
-        -- local origin = playerBomb.Position
-        -- local direction = playerBomb.CFrame.UpVector * 100
-        -- local midpoint = origin + direction/2
-        -- local raycastResult = Workspace:Raycast(origin, direction)
-        -- local killRay = Instance.new("Part")
-        -- killRay.Anchored = true
-        -- killRay.Parent = workspace
-        -- killRay.CFrame = CFrame.new(midpoint,origin)
-        -- killRay.Size = Vector3.new(tileSize,radius, direction.magnitude)
-        local leftRay = createBombRay(playerBomb,"UpVector")
+
+        --  p = plus
+        --  m = minuse
+        local Zp = createBombRay(playerBomb,"UpVector",-1)
+        local Zm = createBombRay(playerBomb,"UpVector", 1)
+
+        --Must change inside function from z to x for position/Cframe/size
+        -- local Xp = createBombRay(playerBomb,"other", 1)
+        -- local Xm = createBombRay(playerBomb,"other", -1)
     end
 
 end)
---[[
-dropMelon.OnServerEvent:connect(function(player,hit) --Params from dropBomb.client.lua
-    local x = hit:GetAttribute("x")
-    local z = hit:GetAttribute("z")
-    
-    if x and z then
-        hit:SetAttribute("isOccupied",true)
-        print(x .." | " ..z)
-        local playerBomb = createBomb()
-        playerBomb.CFrame = hit.CFrame + Vector3.new(0,4,0)
-        Debris:AddItem(playerBomb,2.1)
-        
-        
-        for _,v in pairs(game.Workspace.Tiles:GetChildren()) do
-            local vx = v:GetAttribute("x")
-            local vz = v:GetAttribute("z")
-            if vx and vz then
-                
-                --Loop that iterates the value of players power and captures
-                --the neighbouring cells
-                for i = 1,power do
-                    if ((vx == x+tileSize*i) and (vz == z)) or
-                        ((vx == x-tileSize*i) and (vz == z)) or
-                        ((vz == z+tileSize*i) and (vx == x)) or
-                        ((vz == z-tileSize*i) and (vx == x))
-                    then
-                        table.insert(neighbours,v)
-                    end
-                end
-                table.insert(neighbours,hit)
-            end
-        end
-
-         handleNeighbours()
-
-       -- testTouch() 
-        
-        hit:SetAttribute("isOccupied",false)
-    end
-end)
-]]
